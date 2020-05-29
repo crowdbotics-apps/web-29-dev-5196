5840cdec51e643de3a8d60ceed6bd3d4f1b5b339
Crowdbotics <team@crowdbotics.com>
2020-05-29 12:18:20 +0000

    Update models

---
+++ trggv/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TrggvConfig(AppConfig):
+    name = "trggv"
---
+++ trggv/models.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.db import models
+
+
+class Test(models.Model):
+    "Generated Model"
+    hgfvhgv = models.BigIntegerField()
+
+
+# Create your models here.
